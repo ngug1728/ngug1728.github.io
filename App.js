eb10702540cb6a85143f92ebe06daa731c29b531
Humphrey Ngugi <87475386+ngug1728@users.noreply.github.com>
2022-11-12 12:12:48 +0300

    Framework

--- App.js
+++ App.js
@@ -1,1 +1,24 @@
-export default function Page()
+function Page({head,body}){
+return (
+<div>
+  <section>
+  <p>{head}</p>
+  </section>
+  <section>
+  <p>{body}</p>
+  </section>
+  </div>
+);
+}
+
+export default function App(){
+return (
+  <div>
+  <h1>HUMBLE SYSTEMS</h1>
+<Page 
+  head=""
+  body=""
+  />
+  </div>
+);
+}
